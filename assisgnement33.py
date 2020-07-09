096d8acaa6779ecd03d217941e00dc12fd983d3e
FOUED HAMOUDA <foha2001@yahoo.fr>
2020-07-09 12:37:52 +0100

    OK

--- assisgnement33.py
+++ assisgnement33.py
@@ -1,4 +1,4 @@
-score = input("Enter Score: ")
+score = input("Enter Score between 0 and 1: ")
 scores=float(score)
 if scores<1 and scores>0:
     if scores>=0.9:
@@ -11,7 +11,7 @@ if scores<1 and scores>0:
         print('D')
     elif scores<0.6:
         print('F')
-  
+
 else:
     print('error')
     exit()
