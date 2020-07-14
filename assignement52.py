d48a9af7785648b2c139e31149d8458899c6d1a5
FOUED HAMOUDA <foha2001@yahoo.fr>
2020-07-14 11:05:28 +0100

    fixed

---
+++ assignement52.py
@@ -0,0 +1,20 @@
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num is not "done":
+        try:
+    	   numin=int(num)
+        except:
+           print("Invalid input")
+    if largest is None:
+        largest=numin
+    elif largest<numin:
+        largest=numin
+    if smallest is None:
+    	smallest= numin
+    elif numin<smallest:
+        smallest=numin
+    if num == "done" : break
+print("Maximum is", largest)
+print("Minimum is", smallest)
